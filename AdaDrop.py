f4123c74781b54f1205b92cccaaf92882075dded
Ali Shirazi-Nejad <alishirazinejad10@gmail.com>
2023-12-26 01:43:23 -0600

    about to run first test

--- AdaDrop.py
+++ AdaDrop.py
@@ -1,48 +1,49 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from enum import Enum
+from einops import reduce
 
-class ScalingMethod(Enum):
-    INVERSE = "inverse"
-    SOFTMAX = "softmax"
-    NORMALIZED = "norm"
-
-class AdaDrop_L(nn.Module):
-    def __init__(self, scaling: ScalingMethod) -> None:
+class AdaDrop(nn.Module):
+    def __init__(self, layer, scaling: str, N: int) -> None:
         super().__init__()
-        self.gradients = None
+        self.recent_gradients = []
+        self.max_length = N
         self.scaling = scaling
+        self.valid_scalings = ["inverse", "softmax", "norm"]
+        self._register_gradient_hook(layer)
 
     def _get_gradient_hook(self):
-        def hook(grad):
-            self.gradients = grad.abs()
+        def hook(grad_input):
+            grad = grad_input[0].abs()
+            self.recent_gradients.append(grad)
+            if len(self.recent_gradients) > self.max_length:
+                self.recent_gradients.pop(0)
         return hook
 
-    def register_gradient_hook(self, layer):
-        """
-        Register a hook to the given layer to capture gradients.
-        """
+    def _register_gradient_hook(self, layer):
         layer.register_backward_hook(self._get_gradient_hook())
 
     def _calculate_dropout_probabilities(self):
-        if self.scaling == ScalingMethod.INVERSE:
-            normalized_gradients = self.gradients / self.gradients.sum()
+        if not self.recent_gradients:
+            return None
+
+        gradients = reduce(torch.stack(self.recent_gradients), 'n c -> c', 'mean')
+
+        if self.scaling == "inverse":
+            normalized_gradients = gradients / gradients.sum()
             return 1 - normalized_gradients
 
-        elif self.scaling == ScalingMethod.SOFTMAX:
-            return F.softmax(self.gradients, dim=0)
+        elif self.scaling == "softmax":
+            return F.softmax(gradients, dim=0)
 
-        elif self.scaling == ScalingMethod.NORMALIZED:
-            max_grad = self.gradients.max()
-            return self.gradients / max_grad if max_grad > 0 else self.gradients
-
-        else:
-            raise ValueError(f"Invalid scaling method: {self.scaling}")
+        elif self.scaling == "norm":
+            max_grad = gradients.max()
+            return gradients / max_grad if max_grad > 0 else gradients
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        if self.training and self.gradients is not None:
+        if self.training:
             dropout_probabilities = self._calculate_dropout_probabilities()
-            mask = torch.bernoulli(dropout_probabilities).to(input.device)
-            return input * mask
+            if dropout_probabilities is not None:
+                mask = torch.bernoulli(dropout_probabilities).to(input.device)
+                return input * mask
         return input
--- main.py
+++ main.py
@@ -19,6 +19,9 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader
+
+from AdaDrop import AdaDrop
 
 import einops
 
@@ -44,7 +47,7 @@ class CNN_noDO(nn.Module):
         self.layer3 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
         self.fc1 = nn.Linear(in_features=2048, out_features=512)
@@ -57,9 +60,9 @@ class CNN_noDO(nn.Module):
         x = self.layer3(x)
         x = einops.rearrange(x, 'b c h w -> b (c h w)')
         x = self.fc1(x)
-        x = self.relu(x)
+        x = F.relu(x)
         x = self.fc2(x)
-        x = self.relu(x)
+        x = F.relu(x)
         output = self.fc3(x)
         return output
 
@@ -82,7 +85,7 @@ class CNN_regDO(nn.Module):
         self.layer3 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
         self.dropout = nn.Dropout(p=0.25)
@@ -97,55 +100,194 @@ class CNN_regDO(nn.Module):
         x = einops.rearrange(x, 'b c h w -> b (c h w)')
         x = self.dropout(x)
         x = self.fc1(x)
-        x = self.relu(x)
+        x = F.relu(x)
         x = self.dropout(x)
         x = self.fc2(x)
-        x = self.relu(x)
+        x = F.relu(x)
         x = self.dropout(x)
         output = self.fc3(x)
         return output
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+class CNN_AdaDrop_inverse(nn.Module):
+    def __init__(self):
+        super(CNN_AdaDrop_inverse, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 10)
+        self.adaDrop1 = AdaDrop(self.fc1, scaling="inverse", N=1)
+        self.adaDrop2 = AdaDrop(self.fc2, scaling="inverse", N=1)
+
+    def forward(self, input):
+        x = self.layer1(input)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = einops.rearrange(x, 'b c h w -> b (c h w)')
+        x = self.AdaDrop(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.AdaDrop(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.AdaDrop(x)
+        output = self.fc3(x)
+        return output
+
+
+class CNN_AdaDrop_softmax(nn.Module):
+    def __init__(self):
+        super(CNN_AdaDrop_softmax, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 10)
+        self.adaDrop1 = AdaDrop(self.fc1, scaling="softmax", N=1)
+        self.adaDrop2 = AdaDrop(self.fc2, scaling="softmax", N=1)
+
+    def forward(self, input):
+        x = self.layer1(input)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = einops.rearrange(x, 'b c h w -> b (c h w)')
+        x = self.AdaDrop(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.AdaDrop(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.AdaDrop(x)
+        output = self.fc3(x)
+        return output
+
+
+class CNN_AdaDrop_norm(nn.Module):
+    def __init__(self):
+        super(CNN_AdaDrop_norm, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+
+        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 10)
+        self.adaDrop1 = AdaDrop(self.fc1, scaling="norm", N=1)
+        self.adaDrop2 = AdaDrop(self.fc2, scaling="norm", N=1)
+
+    def forward(self, input):
+        x = self.layer1(input)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = einops.rearrange(x, 'b c h w -> b (c h w)')
+        x = self.AdaDrop(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.AdaDrop(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.AdaDrop(x)
+        output = self.fc3(x)
+        return output
+
+
+def train(args, model, device, train_loader, optimizer, epoch, log_file):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+    for batch_idx, (inputs, labels) in enumerate(train_loader):
+        inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
 
-        output = model(data)
-        loss = F.nll_loss(output, target)
+        output = model(inputs)
+        loss = F.nll_loss(output, labels)
 
         loss.backward()
 
         optimizer.step()
 
         if batch_idx % args.log_interval == 0:
-            print(
-                f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100.0 * batch_idx / len(train_loader)}%)]\tLoss: {loss.item()}")
+            log_file.write(
+                f"Train Epoch: {epoch} [{batch_idx * len(inputs)}/{len(train_loader.dataset)} ({100.0 * batch_idx / len(train_loader)}%)]\tLoss: {loss.item()}\n")
 
             if args.dry_run:
                 break
 
 
-def test(model, device, test_loader):
+def test(model, device, testloader, log_file):
     model.eval()
     total_test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for data, target in testloader:
             data, target = data.to(device), target.to(device)
             output = model(data)
             total_test_loss += F.nll_loss(output, target, reduction='sum').item()
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    total_test_loss /= len(test_loader.dataset)
+    total_test_loss /= len(testloader.dataset)
 
-    print(
-        f"\nTest set: Average loss: {total_test_loss}, Accuracy: {correct}/{len(test_loader.dataset)} ({100.0 * correct / len(test_loader.dataset)}%)\n")
+    log_file.write(
+        f"\nTest set: Average loss: {total_test_loss}, Accuracy: {correct}/{len(testloader.dataset)} ({100.0 * correct / len(testloader.dataset)}%)\n")
 
 
 def main():
+    valid_datasets = {"MNIST", "CIFAR10"}
+
+    valid_models = {
+        "CNN_noDO": CNN_noDO(),
+        "CNN_regDO": CNN_regDO(),
+        "CNN_AdaDrop_inverse": CNN_AdaDrop_inverse(),
+        "CNN_AdaDrop_softmax": CNN_AdaDrop_softmax(),
+        "CNN_AdaDrop_norm": CNN_AdaDrop_norm()
+    }
+
     parser = argparse.ArgumentParser(description="AdaDrop Training and Testing")
 
     parser.add_argument('--dataset', type=str, metavar='DS',
@@ -186,6 +328,8 @@ def main():
 
     args = parser.parse_args()
 
+    assert args.dataset in valid_datasets, "Failed to provide valid dataset: MNIST, CIFAR10"
+
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
     np.random.seed(args.seed)
@@ -199,23 +343,57 @@ def main():
 
     if use_cuda:
         device = torch.device("cuda")
-    if use_mps:
+    elif use_mps:
         device = torch.device("mps")
     else:
         device = torch.device("cpu")
 
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
+    if args.dataset == "MNIST":
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        trainset = datasets.MNIST(root='./data',
+                                  train=True,
+                                  download=True,
+                                  transform=transform)
+        testset = datasets.MNIST(root='./data',
+                                 train=False,
+                                 download=True,
+                                 transform=transform)
 
-    if use_cuda:
-        cuda_kwargs = {'num_workers': 1,
-                       'pin_memory': True,
-                       'shuffle': True}
-        train_kwargs.update(cuda_kwargs)
-        test_kwargs.update(cuda_kwargs)
+    elif args.dataset == "CIFAR10":
+        transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        trainset = datasets.CIFAR10(root='./data',
+                                    train=True,
+                                    download=True,
+                                    transform=transform)
+        testset = datasets.CIFAR10(root='./data',
+                                   train=False,
+                                   download=True,
+                                   transform=transform)
 
+    trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
+    testloader = DataLoader(testset, batch_size=args.batch_size, shuffle=False)
 
+    for model_name, model in valid_models.items():
+        model.to(device)
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+        scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
 
+        with open(f"{model_name}_log.log", "w") as log_file:
+            for epoch in range(1, args.epochs + 1):
+                train(args, model, device, trainloader, optimizer, epoch, log_file)
+                test(model, device, testloader, log_file)
+                scheduler.step()
+
+            if args.save_model:
+                torch.save(model.state_dict(), f"{args.dataset}_{model_name}.pt")
 
 
 if __name__ == "__main__":
