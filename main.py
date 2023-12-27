11b29710b40100066da308de509a17e1c9e1f35d
Ali Shirazi-Nejad <alishirazinejad10@gmail.com>
2023-12-27 15:27:10 -0600

    i want her so bad

--- main.py
+++ main.py
@@ -17,16 +17,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.optim.lr_scheduler import StepLR
+import torch.nn.init as init
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 from AdaDrop import AdaDrop
+
+from torch.utils.tensorboard import SummaryWriter
 
 import einops
 
 import numpy as np
 import random
+
+
+def init_weights(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+            init.constant_(m.bias, 0)
 
 
 class CNN_noDO(nn.Module):
@@ -50,20 +60,31 @@ class CNN_noDO(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
-        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.fc1 = nn.Linear(in_features=128, out_features=512)
         self.fc2 = nn.Linear(512, 128)
         self.fc3 = nn.Linear(128, 10)
+
+        self.layer1.apply(init_weights)
+        self.layer2.apply(init_weights)
+        self.layer3.apply(init_weights)
+        self.fc1.apply(init_weights)
+        self.fc2.apply(init_weights)
+        self.fc3.apply(init_weights)
 
     def forward(self, input):
         x = self.layer1(input)
         x = self.layer2(x)
         x = self.layer3(x)
+        x = self.avg_pool(x)
         x = einops.rearrange(x, 'b c h w -> b (c h w)')
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
         x = F.relu(x)
-        output = self.fc3(x)
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
         return output
 
 
@@ -88,15 +109,25 @@ class CNN_regDO(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
         self.dropout = nn.Dropout(p=0.25)
-        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.fc1 = nn.Linear(in_features=128, out_features=512)
         self.fc2 = nn.Linear(512, 128)
         self.fc3 = nn.Linear(128, 10)
+
+        self.layer1.apply(init_weights)
+        self.layer2.apply(init_weights)
+        self.layer3.apply(init_weights)
+        self.fc1.apply(init_weights)
+        self.fc2.apply(init_weights)
+        self.fc3.apply(init_weights)
 
     def forward(self, input):
         x = self.layer1(input)
         x = self.layer2(x)
         x = self.layer3(x)
+        x = self.avg_pool(x)
         x = einops.rearrange(x, 'b c h w -> b (c h w)')
         x = self.dropout(x)
         x = self.fc1(x)
@@ -105,7 +136,8 @@ class CNN_regDO(nn.Module):
         x = self.fc2(x)
         x = F.relu(x)
         x = self.dropout(x)
-        output = self.fc3(x)
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
         return output
 
 
@@ -130,16 +162,26 @@ class CNN_AdaDrop_inverse(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
-        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.fc1 = nn.Linear(in_features=128, out_features=512)
         self.fc2 = nn.Linear(512, 128)
         self.fc3 = nn.Linear(128, 10)
         self.adaDrop1 = AdaDrop(self.fc1, scaling="inverse", N=1)
         self.adaDrop2 = AdaDrop(self.fc2, scaling="inverse", N=1)
 
+        self.layer1.apply(init_weights)
+        self.layer2.apply(init_weights)
+        self.layer3.apply(init_weights)
+        self.fc1.apply(init_weights)
+        self.fc2.apply(init_weights)
+        self.fc3.apply(init_weights)
+
     def forward(self, input):
         x = self.layer1(input)
         x = self.layer2(x)
         x = self.layer3(x)
+        x = self.avg_pool(x)
         x = einops.rearrange(x, 'b c h w -> b (c h w)')
         x = self.AdaDrop(x)
         x = self.fc1(x)
@@ -148,7 +190,8 @@ class CNN_AdaDrop_inverse(nn.Module):
         x = self.fc2(x)
         x = F.relu(x)
         x = self.AdaDrop(x)
-        output = self.fc3(x)
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
         return output
 
 
@@ -173,16 +216,26 @@ class CNN_AdaDrop_softmax(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
-        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.fc1 = nn.Linear(in_features=128, out_features=512)
         self.fc2 = nn.Linear(512, 128)
         self.fc3 = nn.Linear(128, 10)
         self.adaDrop1 = AdaDrop(self.fc1, scaling="softmax", N=1)
         self.adaDrop2 = AdaDrop(self.fc2, scaling="softmax", N=1)
 
+        self.layer1.apply(init_weights)
+        self.layer2.apply(init_weights)
+        self.layer3.apply(init_weights)
+        self.fc1.apply(init_weights)
+        self.fc2.apply(init_weights)
+        self.fc3.apply(init_weights)
+
     def forward(self, input):
         x = self.layer1(input)
         x = self.layer2(x)
         x = self.layer3(x)
+        x = self.avg_pool(x)
         x = einops.rearrange(x, 'b c h w -> b (c h w)')
         x = self.AdaDrop(x)
         x = self.fc1(x)
@@ -191,7 +244,8 @@ class CNN_AdaDrop_softmax(nn.Module):
         x = self.fc2(x)
         x = F.relu(x)
         x = self.AdaDrop(x)
-        output = self.fc3(x)
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
         return output
 
 
@@ -216,16 +270,26 @@ class CNN_AdaDrop_norm(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
 
-        self.fc1 = nn.Linear(in_features=2048, out_features=512)
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.fc1 = nn.Linear(in_features=128, out_features=512)
         self.fc2 = nn.Linear(512, 128)
         self.fc3 = nn.Linear(128, 10)
         self.adaDrop1 = AdaDrop(self.fc1, scaling="norm", N=1)
         self.adaDrop2 = AdaDrop(self.fc2, scaling="norm", N=1)
 
+        self.layer1.apply(init_weights)
+        self.layer2.apply(init_weights)
+        self.layer3.apply(init_weights)
+        self.fc1.apply(init_weights)
+        self.fc2.apply(init_weights)
+        self.fc3.apply(init_weights)
+
     def forward(self, input):
         x = self.layer1(input)
         x = self.layer2(x)
         x = self.layer3(x)
+        x = self.avg_pool(x)
         x = einops.rearrange(x, 'b c h w -> b (c h w)')
         x = self.AdaDrop(x)
         x = self.fc1(x)
@@ -234,7 +298,8 @@ class CNN_AdaDrop_norm(nn.Module):
         x = self.fc2(x)
         x = F.relu(x)
         x = self.AdaDrop(x)
-        output = self.fc3(x)
+        x = self.fc3(x)
+        output = F.log_softmax(x, dim=1)
         return output
 
 
@@ -253,7 +318,7 @@ def train(args, model, device, train_loader, optimizer, epoch, log_file):
 
         if batch_idx % args.log_interval == 0:
             log_file.write(
-                f"Train Epoch: {epoch} [{batch_idx * len(inputs)}/{len(train_loader.dataset)} ({100.0 * batch_idx / len(train_loader)}%)]\tLoss: {loss.item()}\n")
+                f"Train Epoch: {epoch} [{batch_idx * len(inputs)}/{len(train_loader.dataset)} ({100.0 * batch_idx / len(train_loader):.4f}%)]\tLoss: {loss.item():.4f}\n")
 
             if args.dry_run:
                 break
@@ -305,14 +370,11 @@ def main():
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
 
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--cuda', action='store_true', default=False,
+                        help='enables CUDA training')
 
-    parser.add_argument('--no_cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-
-    parser.add_argument('--no_mps', action='store_true', default=False,
-                        help='disables macOS GPU training')
+    parser.add_argument('--mps', action='store_true', default=False,
+                        help='enables macOS GPU training')
 
     parser.add_argument('--dry_run', action='store_true', default=False,
                         help='quickly check a single pass')
@@ -338,8 +400,8 @@ def main():
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    use_mps = not args.no_mps and torch.backends.mps.is_available()
+    use_cuda = args.cuda and torch.cuda.is_available()
+    use_mps = args.mps and torch.backends.mps.is_available()
 
     if use_cuda:
         device = torch.device("cuda")
@@ -348,10 +410,14 @@ def main():
     else:
         device = torch.device("cpu")
 
+    print(f"Device in use: {device}")
+
     if args.dataset == "MNIST":
         transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.Grayscale(num_output_channels=3),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize((0.5,), (0.5,))
         ])
         trainset = datasets.MNIST(root='./data',
                                   train=True,
@@ -364,6 +430,7 @@ def main():
 
     elif args.dataset == "CIFAR10":
         transform = transforms.Compose([
+            transforms.Resize((224, 224)),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(10),
             transforms.ToTensor(),
@@ -384,9 +451,10 @@ def main():
     for model_name, model in valid_models.items():
         model.to(device)
         optimizer = optim.Adam(model.parameters(), lr=args.lr)
-        scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
 
-        with open(f"{model_name}_log.log", "w") as log_file:
+        scheduler = CosineAnnealingLR(optimizer, T_max=args.epochs)
+
+        with open(f"{model_name}.log", "w") as log_file:
             for epoch in range(1, args.epochs + 1):
                 train(args, model, device, trainloader, optimizer, epoch, log_file)
                 test(model, device, testloader, log_file)
